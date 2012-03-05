ac30b8e2701898b75d33d424ecefa774a877c876
Loris Fichera <loris.fichera@gmail.com>
2012-03-05 20:41:44 +0100

    using erl_img?

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-ERL_PATHS="-pa ebin/"
+ERL_PATHS="-pa ebin/ -pa ../erl_img/ebin"
 
 erlc ${ERL_PATHS} ehtm.rel
 erl ${ERL_PATHS} -boot ehtm
--- src/test.erl
+++ src/test.erl
@@ -4,7 +4,11 @@
 
 -module (test).
 
--export ([go/0]).
+-export ([go/0,
+	  erl_img_test/0
+	 ]).
+
+-include ("erl_img.hrl").
 
 
 data () ->
@@ -19,3 +23,25 @@ data () ->
 go () ->
     Network = network:make_process_name ("MyNetwork"),
     network:feed (Network, data ()).
+
+erl_img_test () ->
+    {ok, Image} = erl_img:load ("/home/loris/101_ObjectCategories/accordion/image_0001.jpg"),
+    PixMap = Image#erl_image.pixmaps,
+    io:format ("Image is ~p~n", [Image]),
+    io:format ("Pixmap is ~p~n", [PixMap]),
+    
+
+    {ok, Image1} = erl_img:load ("/home/loris/101_ObjectCategories/accordion/image_0002.jpg"),
+    PixMap1 = Image1#erl_image.pixmaps,
+    io:format ("Image is ~p~n", [Image1]),
+    io:format ("Pixmap is ~p~n", [PixMap1]),
+    
+    {ok, Image2} = erl_img:load ("/home/loris/Pictures/2012/01/26/DSC_6583.JPG"),
+    PixMap2 = Image2#erl_image.pixmaps,
+    io:format ("Image is ~p~n", [Image2]),
+    io:format ("Pixmap is ~p~n", [PixMap2]).
+    
+    
+    
+    
+    
