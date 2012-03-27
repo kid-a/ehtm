204d802df71185d81e119aa4cbe49a989a682443
Loris Fichera <loris.fichera@gmail.com>
2012-03-27 16:53:45 +0200

    added routine to read both training and test data

---
+++ prototype/utils/read_usps_maltoni.m
@@ -0,0 +1,33 @@
+%% -*- mode: octave -*-p
+function images = read_usps_maltoni (W)
+  classes = [0 1 2 3 4 5 6 7 8 9];
+  images = {};
+
+  switch W
+      
+    case "train"
+      k = 1;
+      for class = classes
+	for j = 1 : 10	  
+	  image = imread(["data/train100/" num2str(class) "/" num2str(j) ".bmp"]);
+	  images{k} = {image, class + 1};
+	  k += 1;
+	endfor
+      endfor
+
+      %% !FIXME "test" not implemented
+    case "test"
+      k = 1;
+      image_number = [359 264 198 166 200 160 170 147 166 177];
+      for class = classes
+	for j = 1 : image_number(class + 1) 
+	  image = imread(["data/test/" num2str(class) "/" num2str(j) ".bmp"]);
+	  images{k} = {image, class + 1};
+	  k += 1;
+	endfor
+      endfor
+
+      
+  endswitch
+
+endfunction
