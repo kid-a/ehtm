4ad57d2868add35d1bfea55ba5744611a51bc087
Loris Fichera <loris.fichera@gmail.com>
2012-03-27 16:06:31 +0200

    fixed bug in propagation

--- prototype/do_propagate.m
+++ prototype/do_propagate.m
@@ -11,22 +11,32 @@ function LAYER2 = do_propagate (LAYER1, LAYER2)
     endfor
   endfor
   
-  %% propagate the messages
-  for i = 1 : layer1_height
-    upper_i = idivide (i, layer2_height);
-    
-    for j = 1 : layer1_width
-      upper_j = idivide (j, layer2_width);
-      
-      msg = LAYER1{i, j}.out_msg;
-      
-      for k = 1 : layer2_height
-	for m = 1 : layer2_width
-	  [_, w] = size (LAYER2{k, m}.in_msg);
-	  LAYER2{k, m}.in_msg{w + 1} = msg;
-	endfor
+  if ((layer2_height == 1) && (layer2_width == 1)) %% if last layer
+    for i = 1 : layer1_height
+      for j = 1: layer1_width
+	msg = LAYER1{i, j}.out_msg;
+	[_, w] = size (LAYER2{1,1}.in_msg);
+	LAYER2{1,1}.in_msg{w + 1} = msg;
       endfor
-
     endfor
-  endfor
+    
+  else
+    
+    %% propagate the messages
+    for i = 1 : layer1_height
+      %%upper_i = idivide (i, layer2_height);
+      upper_i = ceil(i / layer2_height);
+      
+      for j = 1 : layer1_width
+	%%upper_j = idivide (j, layer2_width);
+	upper_j = ceil(j / layer2_width);
+	
+	msg = LAYER1{i, j}.out_msg;
+	
+	[_, w] = size (LAYER2{upper_i, upper_j}.in_msg);
+	LAYER2{upper_i, upper_j}.in_msg{w + 1} = msg;
+		
+      endfor
+    endfor
+  endif
 endfunction
